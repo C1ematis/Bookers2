eee342bab3b78c9abc36adbf516012f0864a622b
Clematis <khtafa@gmail.com>
2021-09-08 14:59:48 +0000

    Update: search

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  get 'search' => "searches#search"
 
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resources :book_comments, only: [:create,:destroy]
